db7f35dc2b80ce7fca6ff703876494159e5b3133
Szasza <szasza@szasza.info>
2017-10-16 08:37:05 +1100

    extracting path registration from init to separate function

--- manifest.js
+++ manifest.js
@@ -4,7 +4,12 @@ const refParser = require('json-schema-ref-parser')
 
 module.exports = {
     version: '1.2.0',
+
+    pluginContext: null,
+
     init: function (pluginContext) {
+        this.pluginContext = pluginContext
+
         const yamlFileContent = fs.readFileSync(pluginContext.settings.definitionFile)
         const definition = yaml.safeLoad(yamlFileContent)
 
@@ -14,48 +19,51 @@ module.exports = {
                 let firstExampleKey = Object.keys(methodDefinition.examples)[0]
                 let example = methodDefinition.examples[firstExampleKey]
 
-                switch(method) {
-                    case 'get':
-                        pluginContext.registerGatewayRoute(app => {
-                            app.get(path, (req, res) => {
-                                res.json(example)
-                            })
-                        })
-                        break
-                    case 'post':
-                        pluginContext.registerGatewayRoute(app => {
-                            app.post(path, (req, res) => {
-                                res.json(example)
-                            })
-                        })
-                        break
-                    case 'patch':
-                        pluginContext.registerGatewayRoute(app => {
-                            app.patch(path, (req, res) => {
-                                res.json(example)
-                            })
-                        })
-                        break
-                    case 'put':
-                        pluginContext.registerGatewayRoute(app => {
-                            app.put(path, (req, res) => {
-                                res.json(example)
-                            })
-                        })
-                        break
-                    case 'delete':
-                        pluginContext.registerGatewayRoute(app => {
-                            app.delete(path, (req, res) => {
-                                res.json(example)
-                            })
-                        })
-                        break
-                }
+                this.registerRoute(path, method, example)
             }
         }
-
-
     },
+
+    registerRoute: function (path, method, example) {
+        switch(method) {
+            case 'get':
+                this.pluginContext.registerGatewayRoute(app => {
+                    app.get(path, (req, res) => {
+                        res.json(example)
+                    })
+                })
+                break
+            case 'post':
+                this.pluginContext.registerGatewayRoute(app => {
+                    app.post(path, (req, res) => {
+                        res.json(example)
+                    })
+                })
+                break
+            case 'patch':
+                this.pluginContext.registerGatewayRoute(app => {
+                    app.patch(path, (req, res) => {
+                        res.json(example)
+                    })
+                })
+                break
+            case 'put':
+                this.pluginContext.registerGatewayRoute(app => {
+                    app.put(path, (req, res) => {
+                        res.json(example)
+                    })
+                })
+                break
+            case 'delete':
+                this.pluginContext.registerGatewayRoute(app => {
+                    app.delete(path, (req, res) => {
+                        res.json(example)
+                    })
+                })
+                break
+        }
+    }
+
     options: {
         definitionFile: {
             type: 'string',
