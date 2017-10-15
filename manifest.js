a80459adfe30c2c3312efdf06d5f9cdb9136fa38
Szasza <szasza@szasza.info>
2017-10-15 19:04:11 +1100

    testing external parser

--- manifest.js
+++ manifest.js
@@ -1,11 +1,16 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
 
+import { parseDefinition } from 'react-lincoln/src/lib/definitions'
+
 module.exports = {
     version: '1.2.0',
     init: function (pluginContext) {
         const yamlFileContent = fs.readFileSync(pluginContext.settings.definitionFile)
-        const definition = yaml.safeLoad(yamlFileContent)
+        const rawDefinition = yaml.safeLoad(yamlFileContent)
+        const definition = parseDefinition(rawDefinition, 'open-api-v3')
+
+        console.log(definition)
 
         for (path in definition.paths) {
             for (method in definition.paths[path]) {
